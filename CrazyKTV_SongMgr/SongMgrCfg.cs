aefe6847e54e6a64035a79b6b28309ce5a3eb27c
Ken Luo <ken670128@hotmail.com>
2015-08-22 04:34:40 +0800

    修正變更資料庫檔案時,歌庫維護頁面裡的自訂語系控制項未更新的問題。

--- CrazyKTV_SongMgr/SongMgrCfg.cs
+++ CrazyKTV_SongMgr/SongMgrCfg.cs
@@ -51,6 +51,9 @@ namespace CrazyKTV_SongMgr
                 // 檢查是否有自訂語系
                 Common_CheckSongLang();
 
+                // 歌庫維護 - 設定自訂語系相關控制項
+                SongMaintenance_SetCustomLangControl();
+
                 // 統計歌曲數量
                 Task.Factory.StartNew(() => Common_GetSongStatisticsTask());
 
