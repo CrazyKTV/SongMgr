8f504145ae24282fb71cc579d45aa26097820b51
Ken Luo <ken670128@hotmail.com>
2015-12-04 07:33:18 +0800

    台語語系關鍵字加入『臺語』、『臺』。

--- CrazyKTV_SongMgr/Program.cs
+++ CrazyKTV_SongMgr/Program.cs
@@ -94,7 +94,7 @@ namespace CrazyKTV_SongMgr
         public static int RemainingSongID = 9999999;
 
         public static List<string> CrazyktvSongLangList = new List<string>() { "國語", "台語", "粵語", "日語", "英語", "客語", "原住民語", "韓語", "兒歌", "其它" };
-        public static List<string> CrazyktvSongLangKeyWordList = new List<string>() { "國語,國", "台語,台,閩南,閩", "粵語,粵,廣東", "日語,日文,日", "英語,英文,英", "客語,客", "原住民語,民謠", "韓語,韓", "兒歌,兒", "其它" };
+        public static List<string> CrazyktvSongLangKeyWordList = new List<string>() { "國語,國", "台語,台,閩南,閩,臺語,臺", "粵語,粵,廣東", "日語,日文,日", "英語,英文,英", "客語,客", "原住民語,民謠", "韓語,韓", "兒歌,兒", "其它" };
         public static List<string> CrazyktvSingerTypeList = new List<string>() { "男歌星", "女歌星", "樂團", "合唱", "外國男", "外國女", "外國樂團", "其它", "未使用", "未使用", "新進歌星" };
         public static List<string> CrazyktvDBTableList = new List<string>();
         public static List<string> CrazyktvDBColumnList = new List<string>();
