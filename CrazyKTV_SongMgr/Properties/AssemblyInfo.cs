8f4d909d2baca0c7704e8d5c386e7b478068109b
Ken Luo <ken670128@hotmail.com>
2018-12-09 18:20:56 +0800

    變更版號。

--- CrazyKTV_SongMgr/Properties/AssemblyInfo.cs
+++ CrazyKTV_SongMgr/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("1.4.8.39")]
+[assembly: AssemblyFileVersion("1.4.8.44")]
